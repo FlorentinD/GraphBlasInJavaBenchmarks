8b8b8b2a0cd05fdb18e19281b1e734b60fb5c558
FlorentinD <florentin.doerre@tu-dresden.de>
2020-11-15 21:08:14 +0100

    Limit nodeCount for triangle count benchmark using JGraphT
    As it would never succeed on larger instances

--- src/jmh/java/org/github/florentind/bench/triangleCount/TriangleCountJGraphTBenchmark.java
+++ src/jmh/java/org/github/florentind/bench/triangleCount/TriangleCountJGraphTBenchmark.java
@@ -3,11 +3,15 @@ package org.github.florentind.bench.triangleCount;
 import org.github.florentind.core.jgrapht.JGraphTConverter;
 import org.jgrapht.Graph;
 import org.jgrapht.GraphMetrics;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.infra.Blackhole;
 
 public class TriangleCountJGraphTBenchmark extends TriangleCountBaseBenchmark {
     Graph jGraph;
+
+    @Param({"10000", "100000"})
+    int nodeCount;
 
     @Setup
     public void setup() {
