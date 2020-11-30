c1e1d52d7174acdc898ac60bca0bb08779f2a159
FlorentinD <florentin.doerre@tu-dresden.de>
2020-11-30 16:57:50 +0100

    Update script to visualize PageRank results

--- benchmarkResults/visualizePageRankResults.py
+++ benchmarkResults/visualizePageRankResults.py
@@ -9,16 +9,29 @@ import pandas as pd
 weighted = False
 
 benchmarkResult = pd.read_csv(
-    "results/{}.csv".format("weightedPageRank/weightedPageRankResult" if weighted else "pageRank/pageRankResults"))
-
+    "results/{}.csv".format("weightedPageRank/weightedPageRankResultsServer" if weighted else "pageRank/pageRankResultsServer"))
 
 print(benchmarkResult.dtypes)
+print(benchmarkResult.head(5))
 
-benchmarkResult["Name"] = benchmarkResult.Benchmark.str.split(".").str[-1]
-benchmarkResult["Name"] = "(" + benchmarkResult.Name + "," + benchmarkResult.concurrency.apply(
+benchmarkResult["Library"] = benchmarkResult.Benchmark.str.split(".").str[-1]
+benchmarkResult["Name"] = "(" + benchmarkResult.Library + "," + benchmarkResult.concurrency.apply(
     str) + ")"
 
-print(benchmarkResult.head(5))
+benchmarkResult[["NodeCount", "Iterations"]] = benchmarkResult.nodeCountIterationCombinations.str.split(";", expand=True)
+# convert strings 0.1M to 100_000
+benchmarkResult["NodeCount"] = benchmarkResult.NodeCount.apply(lambda x: float(x[:-1]))
+benchmarkResult["Iterations"] = benchmarkResult.Iterations.apply(lambda x: int(x))
+
+nodeCountScalingResults = benchmarkResult[benchmarkResult["Iterations"] == 20]
+nodeCountScalingResults["Score"] = benchmarkResult["Score"] / 1000.0
+nodeCountScalingResults["Units"] = "s/op"
+
+iterationScalingResult = benchmarkResult[benchmarkResult["NodeCount"] == 1]
+
+#assert(len(benchmarkResult) + len(benchmarkResult.Name.unique())  == len(nodeCountScalingResults) + len(iterationScalingResult))
+print("{} entries for nodeCounts".format(len(nodeCountScalingResults)))
+print("{} entries for iterations".format(len(iterationScalingResult)))
 
 
 # create diagrams
@@ -29,22 +42,44 @@ print(benchmarkResult.head(5))
 
 # import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 from benchmarkResults.helper import grouped_barplot
 
-# get meta info like units, mode, avg-degree ...
-title = "{} (damping: {}, maxIterations: {}) \n on a random power-law graph with an average degree of {} \n using the {} of {}".format(
-        "Weighted PageRank" if weighted else "PageRank",
-        benchmarkResult.dampingFactor.iloc[0],
-        benchmarkResult.maxIterations.iloc[0],
-        benchmarkResult.avgDegree.iloc[0],
-        benchmarkResult.Mode.iloc[0], benchmarkResult.Cnt.iloc[0]
-    )
+# plot iteration scaling results
+# Idea: x-axis = "iterations" , y-axis = score , hue = library, style = concurrency
+# TODO: also show error?!
 
-fig, ax = plt.subplots()
-
-barplot = grouped_barplot(benchmarkResult, "nodeCount", "Name", "Score", "Error", ax)
-barplot.title(title)
-
-outFile = "out/{}.jpg".format("weightedPageRank" if weighted else "pageRank")
+iterationsPlot = sns.lineplot(data=iterationScalingResult, x="Iterations", y="Score", hue="Library", style="concurrency", markers=True)
+iterationsPlot.set_ylabel("Time in {}".format(iterationScalingResult.Units.unique()[0]))
+iterationsPlot.set_yscale('log')
+outFile = "out/{}_iterationScale.jpg".format("weightedPageRank" if weighted else "pageRank")
 plt.savefig(outFile, bbox_inches='tight')
 plt.show()
+
+
+nodeCountPlot = sns.lineplot(data=nodeCountScalingResults, x="NodeCount", y="Score", hue="Library", style="concurrency", markers=True)
+nodeCountPlot.set_ylabel("Time in {}".format(nodeCountScalingResults.Units.unique()[0]))
+nodeCountPlot.set_xlabel("NodeCount x 10⁶")
+outFile = "out/{}_nodeScale.jpg".format("weightedPageRank" if weighted else "pageRank")
+plt.savefig(outFile, bbox_inches='tight')
+#nodeCountPlot.set_yscale('log')
+plt.show()
+
+
+# get meta info like units, mode, avg-degree ...
+# title = "{} (damping: {}, maxIterations: {}) \n on a random power-law graph with an average degree of {} \n using the {} of {}".format(
+#         "Weighted PageRank" if weighted else "PageRank",
+#         benchmarkResult.dampingFactor.iloc[0],
+#         benchmarkResult.maxIterations.iloc[0],
+#         benchmarkResult.avgDegree.iloc[0],
+#         benchmarkResult.Mode.iloc[0], benchmarkResult.Cnt.iloc[0]
+#     )
+#
+# fig, ax = plt.subplots()
+#
+# barplot = grouped_barplot(benchmarkResult, "nodeCount", "Name", "Score", "Error", ax)
+# barplot.title(title)
+#
+# #outFile = "out/{}.jpg".format("weightedPageRank" if weighted else "pageRank")
+# #plt.savefig(outFile, bbox_inches='tight')
+# plt.show()
